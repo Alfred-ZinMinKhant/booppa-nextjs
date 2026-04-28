d01389c83ef71a710a688dd033cdd018c42352ac
Zin Min Khant <zinminkhant.alfred@gmail.com>
2026-04-28 11:24:20 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' blob: data: api.qrserver.com polygonscan.com amoy.polygonscan.com assets.calendly.com cms.booppa.io 13.229.135.184; font-src 'self' fonts.gstatic.com; connect-src 'self' api.booppa.io cms.booppa.io 13.229.135.184; frame-src 'self' calendly.com;",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
@@ -47,6 +74,14 @@ const nextConfig = {
   },
   async redirects() {
     return [];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
